14b06126ca9dffa9f0b7555919fb10e2ae91b9d9
Maciej Kopeć <maciejpkopec@gmail.com>
2025-02-12 23:49:18 +0100

    Add F keys to the layout

--- keyboards/framework/ansi/keymaps/mpkopec/keymap.c
+++ keyboards/framework/ansi/keymaps/mpkopec/keymap.c
@@ -20,10 +20,10 @@ enum _layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_l0] = LAYOUT(
         KC_DEL,       XXXXXXX,      XXXXXXX,      XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,        XXXXXXX,      L0_LOCK,      DF(_l0),
-        KC_GRV,       KC_1,         KC_2,         KC_3,           KC_4,         KC_5,         KC_6,         KC_7,         KC_8,         KC_9,         KC_0,           KC_MINS,      KC_EQL,       KC_BSPC,
+        KC_GRV,       XXXXXXX,      XXXXXXX,      XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,        XXXXXXX,      XXXXXXX,      KC_BSPC,
         KC_TAB,       KC_Q,         KC_W,         LT(_l1, KC_E),  KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,           KC_LBRC,      KC_RBRC,      KC_BSLS,
-        KC_ESC,       LALT_T(KC_A), LCTL_T(KC_S), RALT_T(KC_D),   LSFT_T(KC_F), KC_G,         KC_H,         RSFT_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), LALT_T(KC_SCLN),KC_QUOT,                    KC_ENT,
-        CW_TOGG,                    KC_Z,         KC_X,           LT(_l2, KC_C),KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,         KC_SLSH,                    XXXXXXX,
+        KC_ESC,       KC_A,         LCTL_T(KC_S), RALT_T(KC_D),   LSFT_T(KC_F), KC_G,         KC_H,         RSFT_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), LALT_T(KC_SCLN),KC_QUOT,                    KC_ENT,
+        CW_TOGG,                    KC_Z,         KC_X,           LT(_l2, KC_C),LALT_T(KC_V), KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,         KC_SLSH,                    XXXXXXX,
         XXXXXXX,      MO(_l9),      KC_LGUI,      XXXXXXX,                      KC_SPC,                                   XXXXXXX,      TT(_l8),      KC_LEFT,        KC_UP,        KC_DOWN,      KC_RGHT
     ),
     [_l1] = LAYOUT(
@@ -35,12 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______,          _______,                   _______, _______, _______, _______, _______, _______
     ),
     [_l2] = LAYOUT(
-        _______, _______, _______, _______,     _______,   _______,   _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______,     _______,   _______,   _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______,     _______,   _______,   KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_PSCR, _______, _______, _______,
-        _______, _______, _______, RCS(KC_TAB), _______,   C(KC_TAB), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,
-        _______,          _______, LSA(KC_TAB), _______,   A(KC_TAB), _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______,                _______,                     _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,   KC_F1,   KC_F2,   KC_F3,     KC_F4,     KC_F5, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,   KC_F6,   KC_F7,   KC_F8,     KC_F9,    KC_F10, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_PSCR, _______, _______, _______,
+        _______,  KC_F11,  KC_F12, RCS(KC_S), C(KC_S),   _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,
+        _______,          _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______,              _______,                   QK_BOOT, _______, _______, _______, _______, _______
     ),
     [_l3] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
